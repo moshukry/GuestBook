d2e2dcaf371eedd3322e2d60e8134055f6047da6
mohamed shukry <mohamed.shukry412@gmail.com>
2022-01-13 00:30:51 +0200

    model validations

--- GuestBook/Models/User.cs
+++ GuestBook/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GuestBook.Models
@@ -10,12 +11,17 @@ namespace GuestBook.Models
         {
             Messages = new HashSet<Message>();
         }
-
         public int UserId { get; set; }
+        [Required(ErrorMessage ="*")]
         public string? UserName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [EmailAddress(ErrorMessage ="Insert a valid email address!")]
         public string? UserEmail { get; set; }
+        [Required(ErrorMessage = "*")]
         public string? Password { get; set; }
         [NotMapped]
+        [Required(ErrorMessage = "*")]
+        [Compare("Password", ErrorMessage ="password dosent match")]
         public string? Confirm_Password { get; set; }
 
         public virtual ICollection<Message> Messages { get; set; }
