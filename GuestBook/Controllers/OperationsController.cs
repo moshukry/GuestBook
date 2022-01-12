24af325f140bb5d36df173886b8cfa1bd042c682
mohamed shukry <mohamed.shukry412@gmail.com>
2022-01-12 21:38:46 +0200

    login page start

---
+++ GuestBook/Controllers/OperationsController.cs
@@ -0,0 +1,19 @@
+﻿using GuestBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBook.Controllers
+{
+    public class OperationsController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LogIn(User user,bool remember)
+        {
+            //User u = 
+            return View();
+        }
+    }
+}
