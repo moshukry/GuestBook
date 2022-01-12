61a7a2f070b6f8195b56c0a7456291a02ec10c0c
mohamed shukry <mohamed.shukry412@gmail.com>
2022-01-13 00:18:33 +0200

    user controller and view init

---
+++ GuestBook/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GuestBook.Models;
+
+namespace GuestBook.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly GuestBookContext db;
+
+        public UsersController(GuestBookContext context)
+        {
+            db = context;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+    }
+}
--- GuestBook/Models/User.cs
+++ GuestBook/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GuestBook.Models
 {
@@ -14,6 +15,8 @@ namespace GuestBook.Models
         public string? UserName { get; set; }
         public string? UserEmail { get; set; }
         public string? Password { get; set; }
+        [NotMapped]
+        public string? Confirm_Password { get; set; }
 
         public virtual ICollection<Message> Messages { get; set; }
     }
