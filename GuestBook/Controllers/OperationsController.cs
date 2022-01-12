9248fdeb6ce63a5ef97f88712b82f0f7ac7682dc
mohamed shukry <mohamed.shukry412@gmail.com>
2022-01-12 21:57:40 +0200

    login cookies

--- GuestBook/Controllers/OperationsController.cs
+++ GuestBook/Controllers/OperationsController.cs
@@ -5,15 +5,42 @@ namespace GuestBook.Controllers
 {
     public class OperationsController : Controller
     {
+        private readonly GuestBookContext db;
+
+        public OperationsController(GuestBookContext db)
+        {
+            this.db = db;
+        }
         public IActionResult LogIn()
         {
+            string id = Request.Cookies["userId"];
+            if (id != null)
+            {
+                HttpContext.Session.SetString("UserId", id);
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
         [HttpPost]
         public IActionResult LogIn(User user,bool remember)
         {
-            //User u = 
-            return View();
+            User u = db.Users.Where(n=>n.UserEmail == user.UserEmail && n.Password == user.Password).FirstOrDefault();
+            if (u != null)
+            {
+                if (remember)
+                {
+                    CookieOptions opt = new CookieOptions();
+                    opt.Expires = DateTime.Now.AddDays(10);
+                    Response.Cookies.Append("id", u.UserId.ToString(), opt);
+                }
+                HttpContext.Session.SetString("UserId", u.UserId.ToString());
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewBag.wrong = "Wrond Email or Password!!";
+            }
+            return RedirectToAction("LogIn");
         }
     }
 }
