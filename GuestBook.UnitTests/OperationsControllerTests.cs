bfe61cdd08f5c72664a7eb2427d4960335b1f253
mohamed shukry <mohamed.shukry412@gmail.com>
2022-01-13 16:43:51 +0200

    operation test begin

---
+++ GuestBook.UnitTests/OperationsControllerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuestBook.UnitTests
+{
+    [TestClass]
+    public class OperationsControllerTests
+    {
+        [TestMethod]
+        public void LogIn_LoggedHasCookies_RedirectToMessages()
+        {
+
+        }
+    }
+}
