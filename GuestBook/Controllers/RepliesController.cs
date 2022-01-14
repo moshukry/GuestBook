b047cdbf5fb1bbc90a92e98caeea8134df382748
mohamed shukry <mohamed.shukry412@gmail.com>
2022-01-14 02:25:58 +0200

    reply init

---
+++ GuestBook/Controllers/RepliesController.cs
@@ -0,0 +1,74 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GuestBook.Models;
+
+namespace GuestBook.Controllers
+{
+    public class RepliesController : Controller
+    {
+        private readonly GuestBookContext db;
+
+        public RepliesController(GuestBookContext context)
+        {
+            db = context;
+        }
+        public IActionResult Create(int message_id)
+        {
+            Reply reply = new Reply();
+            reply.MessageId = message_id;
+            return View(reply);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(int message_id,Reply reply)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(reply);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MessageId"] = new SelectList(db.Messages, "MessageId", "MessageBody", reply.MessageId);
+            return View(reply);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reply = await db.Replies
+                .Include(r => r.Message)
+                .FirstOrDefaultAsync(m => m.ReplyId == id);
+            if (reply == null)
+            {
+                return NotFound();
+            }
+
+            return View(reply);
+        }
+
+        // POST: Replies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reply = await db.Replies.FindAsync(id);
+            db.Replies.Remove(reply);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReplyExists(int id)
+        {
+            return db.Replies.Any(e => e.ReplyId == id);
+        }
+    }
+}
